78d5c65ae7014d8563c704601f64b91dd84b3351
Beki <126163629+VelvetBat@users.noreply.github.com>
2024-01-10 18:22:09 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,6 +17,8 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
   tempElement.innerHTML = Math.round(temp);
+
+  getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -39,7 +41,7 @@ function formatDate(date) {
 
   return `${day} ${hours}:${minutes}`;
 }
-
+//api for current weather
 function searchCity(city) {
   let apiKey = "od4bd0df154ad3f5258f65d3a3c5bt61";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
@@ -53,26 +55,48 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
-function displayForecast() {
+function formatDay(timestamp) {
+  let date = new date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+//api for forecast
+function getForecast(city) {
+  let apiKey = "od4bd0df154ad3f5258f65d3a3c5bt61";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
   let forecastElement = document.querySelector("#forecast");
 
   let days = ["Tue", "Wed", "Thurs", "Fri", "Sat", "Sun", "Mon"];
   let forecastHtml = "";
 
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
-<div class="forecast-day">
-  <div class="weather-forecast-date">${day}</div>
-            🌧️ 
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+<div class="forecast-day"></div>
+  <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <img src="${
+              day.condition.icon_url
+            }" class="weather-forecast-icon" /></div>
             <div class="weather-forecast-temperatures">
-              <span class="weather-forecast-temperature-max">18°C </span
-              ><span class="weather-forecast-temperature-min">12°C </span>
+              <span class="weather-forecast-temperature-max">${Math.round(
+                day.temperature.maximum
+              )}°C </span
+              ><span class="weather-forecast-temperature-min">${Math.round(
+                day.temperature.minimum
+              )}°C </span>
             </div>
           </div>
         </div>
         `;
+    }
   });
   forecastElement.innerHTML = forecastHtml;
 }
@@ -81,4 +105,3 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Sydney");
-displayForecast();
